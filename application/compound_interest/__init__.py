9cd6219e971bf56061409e73b0d3825142e532f3
BinyaminR <binyamin.rothenberg@gmail.com>
2023-09-27 17:36:03 +0300

    fix

---
+++ application/compound_interest/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from pymongo import MongoClient
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_wtf import CSRFProtect
+import os
+
+
+app = Flask(__name__)
+secret_key = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = secret_key 
+if secret_key is None:
+    app.config['SECRET_KEY'] = "key_for_pytest_jonathan&binyamin"
+
+csrf = CSRFProtect(app)
+
+hostname = os.environ.get("HOST_NAME")
+username = os.environ.get("MONGO_DB_USERNAME")
+password = os.environ.get("MONGO_DB_PASSWORD")
+MONGO_URI = f"mongodb://{username}:{password}@{hostname}/"
+if hostname is not None or username is not None or password is not None:
+    client = MongoClient(MONGO_URI)
+else:
+    client = MongoClient("mongodb://localhost:27017")
+
+
+db = client["ProjectDB"]
+users = db["users"]
+investments = db["investments"]
+
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+
+from compound_interest import routes
